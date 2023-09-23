e266e7654b84ece472f243f5258124e44d80d406
George Davidson <georgedavidson392@gmail.com>
2023-09-23 15:36:57 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Auto Clicker GUI
+
+An auto-clicker made with customtkinter.
+
+![auto_clicker_gui.png](README.assets/auto_clicker_gui.png)
+
+## Python Version
+
+This project was developed using Python 3.10.10.
+
+## Install Dependencies
+
+Install the necessary dependencies.
+
+```text
+pip install -r requirements.txt
+```
+
+## How To Use
+
+Set the click delay to get different times between mouse clicks.
+
+Set the random offset to give the time between clicks some randomness. If the click delay is 0.4 and the random offset is 0.2, the click delay will be a random number between 0.2 and 0.6 seconds.
+
+Set a hotkey for each of the different mouse buttons.
+
+## Things To Note
+
+To set letters, symbols, and numbers to hotkeys, they will need an apostrophe around them. Examples: 'x', '7', and '='.
+
+Modifier and function Key assignments must begin with the word "Key". Examples: Key.f6, Key.shift, Key.shift_r, Key.ctrl_l, and Key.enter.
+
+This project is meant to show off the GUI, I don't know why you would use an auto clicker made in Python.
---
+++ main.py
@@ -0,0 +1,293 @@
+import os
+import random
+import threading
+import time
+import tkinter
+
+import customtkinter
+import pynput
+
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("blue")
+
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("500x300")
+        self.resizable(0, 0)
+        self.title("Auto Clicker")
+        self.iconphoto(False, tkinter.PhotoImage(
+            file=os.path.join("icon", "icon.png")))
+
+        self.left_hotkey = customtkinter.StringVar(self, "'x'")
+        self.right_hotkey = customtkinter.StringVar(self, "Key.ctrl_l")
+
+        self.delay = customtkinter.StringVar(self, f"Click Delay (0.06)")
+        self.offset = customtkinter.StringVar(self, f"Random Offset (0.04)")
+
+        self.top_frame = customtkinter.CTkFrame(master=self)
+        self.top_frame.place(
+            anchor="n",
+            x=250,
+            y=10, width=480,
+            height=120)
+
+        self.delay_lable = customtkinter.CTkLabel(
+            master=self.top_frame,
+            textvariable=self.delay)
+        self.delay_lable.place(anchor="n", x=240, y=10)
+
+        self.delay_slider = customtkinter.CTkSlider(
+            master=self.top_frame,
+            from_=10,
+            to=1000,
+            command=lambda delay: self.delay.set(f"Click Delay ({round(delay / 1000, 2)})"))
+        self.delay_slider.place(
+            anchor="n",
+            x=240,
+            y=40,
+            width=460,
+            height=20)
+        self.delay_slider.set(60)
+
+        self.offset_lable = customtkinter.CTkLabel(
+            master=self.top_frame,
+            textvariable=self.offset)
+        self.offset_lable.place(anchor="n", x=240, y=60)
+
+        self.offset_slider = customtkinter.CTkSlider(
+            master=self.top_frame,
+            from_=0,
+            to=1000,
+            command=lambda delay: self.offset.set(f"Random Offset ({round(delay / 1000, 2)})"))
+        self.offset_slider.place(
+            anchor="n",
+            x=240,
+            y=90,
+            width=460,
+            height=20)
+        self.offset_slider.set(40)
+
+        self.middle_frame_top = customtkinter.CTkFrame(master=self)
+        self.middle_frame_top.place(
+            anchor="center",
+            x=250,
+            y=175,
+            width=480,
+            height=70)
+
+        self.left_main_button_frame = customtkinter.CTkFrame(master=self.middle_frame_top)
+        self.left_main_button_frame.place(
+            anchor="w",
+            x=10,
+            y=35,
+            width=225,
+            height=50)
+
+        self.left_button_frame = customtkinter.CTkFrame(master=self.left_main_button_frame)
+        self.left_button_frame.place(
+            anchor="w",
+            x=10, y=25,
+            width=97.5,
+            height=30)
+
+        self.left_button_lable = customtkinter.CTkLabel(
+            master=self.left_button_frame,
+            text="Hotkey")
+        self.left_button_lable.place(
+            anchor="center",
+            x=48.75,
+            y=15,
+            width=97.5,
+            height=20)
+
+        self.left_hotkey_entry = customtkinter.CTkEntry(
+            master=self.left_main_button_frame,
+            textvariable=self.left_hotkey)
+        self.left_hotkey_entry.place(
+            anchor="e",
+            x=215,
+            y=25,
+            width=97.5,
+            height=30)
+
+        self.left_main_mouse_button_frame = customtkinter.CTkFrame(master=self.middle_frame_top)
+        self.left_main_mouse_button_frame.place(
+            anchor="e",
+            x=470,
+            y=35,
+            width=225,
+            height=50)
+
+        self.left_mouse_button_frame = customtkinter.CTkFrame(master=self.left_main_mouse_button_frame)
+        self.left_mouse_button_frame.place(
+            anchor="w",
+            x=10, y=25,
+            width=97.5,
+            height=30)
+
+        self.left_mouse_button_lable = customtkinter.CTkLabel(
+            master=self.left_mouse_button_frame,
+            text="Mouse Button")
+        self.left_mouse_button_lable.place(
+            anchor="center",
+            x=48.75,
+            y=15,
+            width=97.5,
+            height=20)
+
+        self.left_button_label = customtkinter.CTkLabel(
+            master=self.left_main_mouse_button_frame,
+            text="Left")
+        self.left_button_label.place(
+            anchor="e",
+            x=215,
+            y=25,
+            width=97.5,
+            height=30)
+
+        self.middle_frame_bottom = customtkinter.CTkFrame(master=self)
+        self.middle_frame_bottom.place(
+            anchor="center",
+            x=250,
+            y=255,
+            width=480,
+            height=70)
+
+        self.right_main_button_frame = customtkinter.CTkFrame(master=self.middle_frame_bottom)
+        self.right_main_button_frame.place(
+            anchor="w",
+            x=10,
+            y=35,
+            width=225,
+            height=50)
+
+        self.right_button_frame = customtkinter.CTkFrame(master=self.right_main_button_frame)
+        self.right_button_frame.place(
+            anchor="w",
+            x=10,
+            y=25,
+            width=97.5,
+            height=30)
+
+        self.right_button_lable = customtkinter.CTkLabel(
+            master=self.right_button_frame,
+            text="Hotkey")
+        self.right_button_lable.place(
+            anchor="center",
+            x=48.75,
+            y=15,
+            width=97.5,
+            height=20)
+
+        self.right_hotkey_entry = customtkinter.CTkEntry(
+            master=self.right_main_button_frame,
+            textvariable=self.right_hotkey)
+        self.right_hotkey_entry.place(
+            anchor="e",
+            x=215,
+            y=25,
+            width=97.5,
+            height=30)
+
+        self.right_main_mouse_button_frame = customtkinter.CTkFrame(
+            master=self.middle_frame_bottom)
+        self.right_main_mouse_button_frame.place(
+            anchor="e",
+            x=470,
+            y=35,
+            width=225,
+            height=50)
+
+        self.right_mouse_button_frame = customtkinter.CTkFrame(
+            master=self.right_main_mouse_button_frame)
+        self.right_mouse_button_frame.place(
+            anchor="w",
+            x=10,
+            y=25,
+            width=97.5,
+            height=30)
+
+        self.right_mouse_button_lable = customtkinter.CTkLabel(
+            master=self.right_mouse_button_frame,
+            text="Mouse Button")
+        self.right_mouse_button_lable.place(
+            anchor="center",
+            x=48.75,
+            y=15,
+            width=97.5,
+            height=20)
+
+        self.right_button_label = customtkinter.CTkLabel(
+            master=self.right_main_mouse_button_frame,
+            text="right")
+        self.right_button_label.place(
+            anchor="e",
+            x=215,
+            y=25,
+            width=97.5,
+            height=30)
+
+
+class Clicker:
+    def __init__(self):
+        self.left_clicking = False
+        self.right_clicking = False
+
+    def calculate_delay(self, app):
+        subtracted_offset = round(
+            (float(app.delay.get()[13:-1]) - float(app.offset.get()[15:-1])) * 1000)
+        added_offset = round(
+            (float(app.delay.get()[13:-1]) + float(app.offset.get()[15:-1])) * 1000)
+
+        if subtracted_offset < 0.01:
+            subtracted_offset = 0.01
+
+        if subtracted_offset > added_offset:
+            subtracted_offset = added_offset
+
+        return random.randint(round(subtracted_offset), round(added_offset)) / 1000
+
+    def clicker(self, app):
+        while True:
+            if self.left_clicking:
+                pynput.mouse.Controller().click(pynput.mouse.Button.left)
+
+            if self.right_clicking:
+                pynput.mouse.Controller().click(pynput.mouse.Button.right)
+
+            time.sleep(self.calculate_delay(app))
+
+    def on_press(self, key, app):
+        if str(key) == app.left_hotkey.get():
+            self.left_clicking = not self.left_clicking
+
+        if str(key) == app.right_hotkey.get():
+            self.right_clicking = not self.right_clicking
+
+
+def keyboard_listener(app, clicker):
+    with pynput.keyboard.Listener(
+            on_press=lambda key: clicker.on_press(key, app)) as keyboard_listener:
+        keyboard_listener.join()
+
+
+def main():
+    app = App()
+    clicker = Clicker()
+    clicker_thred = threading.Thread(
+        target=clicker.clicker,
+        args=(app,))
+    clicker_thred.daemon = True
+    clicker_thred.start()
+    keyboard_listener_thred = threading.Thread(
+        target=keyboard_listener,
+        args=(app, clicker))
+    keyboard_listener_thred.daemon = True
+    keyboard_listener_thred.start()
+    app.mainloop()
+
+
+if __name__ == "__main__":
+    main()
